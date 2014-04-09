12b1003ca54bb0542e7a5ed42f0d36528fb02687
puwelOus <pavol.dano@gmail.com>
2014-04-09 21:16:24 +0200

    Comment modification. Object instead of array used as param in comment.

--- CI_PP/application/libraries/Pinterest/ICPinterest.php
+++ CI_PP/application/libraries/Pinterest/ICPinterest.php
@@ -16,39 +16,39 @@ interface ICPinterest {
      * Adds required Pinterest keys and assigned values to the response including checking whether the keys
      * are required at all.
      * 
-     * @param array $response_array
-     *  Actual response array to put keys and values into.
+     * @param object $responseObject
+     *  Actual response object to put keys and values into.
      * @param array $key_value_items_array
      *  Array including required key sand values to be added into the response message.
-     * @retval array
-     *  Updated response with a new keys and values in a form of an array.
+     * @retval object
+     *  Updated response object with a new keys and values in a form of an array.
      */
-    public function add_pinterest_required_response_items($response_array, $key_value_items_array);
+    public function add_pinterest_required_response_items($responseObject, $key_value_items_array);
 
     /**
      * Adds optional Pinterest keys and assigned values to the response including checking whether the keys
      * are optional at all.
      * 
-     * @param array $response_array
-     *  Actual response array to put keys and values into.
+     * @param object $responseObject
+     *  Actual response object to put keys and values into.
      * @param array $key_value_items_array
      *  Array including optional keys and values to be added into the response message.
-     * @retval array
-     *  Updated response with a new optional keys and values in a form of an array.
+     * @retval object
+     *  Updated response object with a new optional keys and values in a form of an array.
      */
-    public function add_pinterest_optional_response_items($response_array, $key_value_items_array);
+    public function add_pinterest_optional_response_items($responseObject, $key_value_items_array);
 
     /**
      * Adds any keys and assigned values to the response.
      * 
-     * @param array $response_array
-     *  Actual response array to put keys and values into.
+     * @param object $responseObject
+     *  Actual response object to put keys and values into.
      * @param array $key_value_items_array
      *  Array including any keys and values to be added into the response message.
-     * @retval array
-     *  Updated response with a new (any) keys and values in a form of an array.
+     * @retval object
+     *  Updated response object with a new (any) keys and values in a form of an array.
      */
-    public function add_pinterest_any_response_items($response_array, $key_value_items_array);
+    public function add_pinterest_any_response_items($responseObject, $key_value_items_array);
 }
 
 ?>
